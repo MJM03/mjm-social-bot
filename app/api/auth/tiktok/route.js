e93ff2c769c59639b35d3b69c933d44ea3e4e263
MJM03 <mjm030798@gmail.com>
2026-08-12 05:38:17 -0500

    Request TikTok upload scope

--- app/api/auth/tiktok/route.js
+++ app/api/auth/tiktok/route.js
@@ -13,7 +13,7 @@ export async function GET(request) {
   const url = new URL('https://www.tiktok.com/v2/auth/authorize/');
   url.searchParams.set('client_key', clientKey);
   url.searchParams.set('response_type', 'code');
-  url.searchParams.set('scope', 'user.info.basic');
+  url.searchParams.set('scope', 'user.info.basic,video.upload');
   url.searchParams.set('redirect_uri', redirectUri);
   url.searchParams.set('state', state);
 
